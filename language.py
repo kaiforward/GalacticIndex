820a7ab802180e54940154c820838b47f96bae52
kaiforward <kaiforward123@gmail.com>
2017-08-16 21:25:25 +0100

    Refined Planet name generator

--- language.py
+++ language.py
@@ -12,22 +12,19 @@ def get_language():
 
 
 def create_word(min_verb_, max_verb):
-    # creates a word by getting the verb list and randomly selecting non-repeating verbs to make words
+    # creates a word by getting the syllable list and randomly selecting non-repeating syllable to make words
     words_list = get_language()
+
     verbs_list = words_list[1:184]
     connectors_list = words_list[186:194]
     nouns_list = words_list[196:395]
     norse_lemmas = [verbs_list, connectors_list, nouns_list]
     word = ""  # created word
-    previous_verb = ""  # previous used verb
-    verbs = random.randint(min_verb_, max_verb)
-    for verb in xrange(0, verbs):
-        random_verb = random.choice(norse_lemmas[verb])
-        if previous_verb == random_verb:
-            while previous_verb == random_verb:
-                random_verb = random.choice(verbs_list)
-        previous_verb = random_verb
-        word += random_verb
-        if verb <= 1:
-            word += '-'
+    syllables = random.randint(min_verb_, max_verb)
+
+    for current_syllable in xrange(0, syllables):
+        random_syllable = random.choice(norse_lemmas[current_syllable])  # choose random syllable
+        word += random_syllable
+        word += '-'  # add hyphen between word sections
+    word = word[:-1]  # remove last hyphen
     return word
